881e97f0c36ae3a5daa9e44e2593c992df237fe3
vincentiabelindaa <18223078@std.stei.itb.ac.id>
2024-11-18 16:38:45 +0700

    Update user.h

--- user.h
+++ user.h
@@ -1,21 +1,24 @@
-#ifndef ADT_USER_H
-#define ADT_USER_H
+#ifndef USER_H
+#define USER_H
 
-#define MAX_USERS 100
 #define MAX_LEN 256
+#define MAX_USERS 100
 
 typedef struct {
     char username[MAX_LEN];
     char password[MAX_LEN];
-    int money;
 } User;
 
-// Global Variables
 extern User users[MAX_USERS];
 extern int num_users;
 
-// Function Prototypes
+// Fungsi untuk mendaftar pengguna
 int registerUser(const char *username, const char *password);
-int loginUser(const char *username, const char *password);
+
+// Fungsi untuk memuat data pengguna dari file
+void loadUsersFromFile();
+
+// Fungsi untuk menyimpan data pengguna ke file
+void saveUsersToFile();
 
 #endif
