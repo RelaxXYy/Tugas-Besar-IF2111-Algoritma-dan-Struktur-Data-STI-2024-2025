df6a01ba8c610943914ce46561e512f951ca8d01
vincentiabelindaa <18223078@std.stei.itb.ac.id>
2024-11-21 16:18:57 +0700

    Update register.c

--- register.c
+++ register.c
@@ -4,26 +4,26 @@
 #include "../include/config.h"
 
 void handle_register() {
-    int is_registration_successful = 0; // untuk status registrasi
+    int is_registration_successful = 0; // status registrasi
 
     while (!is_registration_successful) {
         printf(">> REGISTER\n");
 
         // baca username
-        char input_username[MAX_LEN];
+        char input_name[MAX_LEN];
         printf("Username: ");
         adv();       
         start_word();
 
-        // salin current_word.tab_word ke input_username
+        // salin current_word.tab_word ke input_name
         int i = 0;
         while (current_word.tab_word[i] != '\0' && i < MAX_LEN - 1) {
-            input_username[i] = current_word.tab_word[i];
+            input_name[i] = current_word.tab_word[i];
             i++;
         }
-        input_username[i] = '\0';
+        input_name[i] = '\0';
 
-        // validasi username: username hanya boleh satu kata
+        // validasi name: username hanya boleh satu kata
         if (!end_word) { // input mengandung spasi
             printf("Username hanya boleh satu kata. Coba lagi!! \n\n");
             while (get_current_char() != '\n' && get_current_char() != EOF) {
@@ -33,9 +33,9 @@ void handle_register() {
         }
 
         // cek apakah username sudah ada
-        if (is_user_exist(users, input_username)) {
-            printf("Username \"%s\" sudah digunakan. Silakan coba username lain.\n\n", input_username);
-            continue; // masukkin username ulang
+        if (is_user_exist(users, input_name)) {
+            printf("Name \"%s\" sudah digunakan. Silakan coba name lain.\n\n", input_name);
+            continue; 
         }
 
         // baca password
@@ -68,8 +68,8 @@ void handle_register() {
         }
 
         // tambah pengguna baru
-        add_user(users, input_username, input_password, 0); 
-        printf("Akun dengan username \"%s\" berhasil dibuat.\n\n", input_username);
+        add_user(users, input_name, input_password, 0);  // Ganti username jadi name
+        printf("Akun dengan name \"%s\" berhasil dibuat.\n\n", input_name);
         is_registration_successful = 1; // berhasil
     }
 }
