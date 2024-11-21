b38098089c0b4e13e429184188c7926f7449955f
RelaxXYy <19623126@std.stei.itb.ac.id>
2024-11-21 11:00:18 +0700

    initial load

---
+++ src/function/load.c
@@ -0,0 +1,11 @@
+#include "../include/all_library_headers.h"
+#include "../include/all_ADT_headers.h"
+#include "../include/boolean.h"
+#include "../include/config.h"
+
+User users[MAX_USER];
+Barang barangs[MAX_BARANG];
+
+int main(){
+    
+}
