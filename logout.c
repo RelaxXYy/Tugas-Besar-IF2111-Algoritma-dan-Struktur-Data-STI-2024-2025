865af914e3d9bf7f7287a5ea9fa5235b698bb74c
Gabriela Jennifer Sandy <167641624+GbrlJennie@users.noreply.github.com>
2024-11-23 08:39:53 +0700

    Add logout.c

---
+++ logout.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "../include/user.h"
+#include "../include/mesin_kata.h"
+#include "../include/config.h"
+
+void handle_logout(char *current_user) {
+    // Check if a user is logged in
+    if (current_user[0] == '\0') {
+        printf("Tidak ada pengguna yang sedang login. Silakan LOGIN terlebih dahulu.\n");
+        return;
+    }
+
+    // Inform the user about the logout
+    printf("%s telah logout dari sistem PURRMART. Silakan REGISTER/LOGIN kembali untuk melanjutkan.\n", current_user);
+
+    // Clear the current user
+    current_user[0] = '\0';
+}
