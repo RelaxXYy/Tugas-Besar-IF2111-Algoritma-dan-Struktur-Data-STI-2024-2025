1ed116c3843d77ff80593473306b9078222a5dc1
irdinailmuna <18223060@std.stei.itb.ac.id>
2024-11-25 00:06:27 +0700

    Update store list

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "src/include/boolean.h"
 #include "src/include/mesin_kata.h"
 #include "src/include/start.h"
+#include "src/include/storelist.h"
 #include "src/include/quit.h"
 #include <stdio.h>
 
@@ -10,22 +11,22 @@ int main()
 
   while (!isQuit)
   {
-    if (is_same_word(current_word,string_to_word("START"))||is_same_word(current_word,string_to_word("START")))
+    if (is_same_word(current_word, string_to_word("START")) || is_same_word(current_word, string_to_word("start")))
     {
       STARTCOMMAND();
-     // printf("%s\n", currentConfig.tab_word);
+      // printf("%s\n", currentConfig.tab_word);
     }
-    else if (is_same_word(current_word,string_to_word("QUIT"))||is_same_word(current_word,string_to_word("QUIT")))
+    else if (is_same_word(current_word, string_to_word("QUIT")) || is_same_word(current_word, string_to_word("quit")))
     {
       QUITCOMMAND();
     }
-    else if (is_same_word(current_word,string_to_word("STORAGELIST"))||is_same_word(current_word,string_to_word("storagelist")))
+    else if (currentConfig.length > 0 && (is_same_word(current_word, string_to_word("STORE LIST")) || is_same_word(current_word, string_to_word("store list"))))
     {
-      
+      STORELISTCOMMAND();
     }
     else
     {
-
+      // printf("%s\n", current_word.tab_word);
       printf("Perintah yang anda masukan tidak valid!\n");
     }
     adv_word();
---
+++ src/include/storelist.h
@@ -0,0 +1,7 @@
+
+#ifndef __STORELIST_H_
+#define ____STORELIST_H__H_
+
+void STORELISTCOMMAND();
+
+#endif
