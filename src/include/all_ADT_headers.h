472c2af0f547025f38a4fe910dc792184f7fe01e
RelaxXYy <165752442+RelaxXYy@users.noreply.github.com>
2024-11-25 20:50:10 +0700

    upload include

---
+++ src/include/all_ADT_headers.h
@@ -0,0 +1,20 @@
+#ifndef ALL_ADT_HEADERS_H
+#define ALL_ADT_HEADERS_H
+
+#include "queue.h"
+#include "list.h"
+#include "user.h"
+#include "mesin_karakter.h"
+#include "mesin_kata.h"
+#include "barang.h"
+#include "start.h"
+#include "load.h"
+#include "save.h"
+#include "console.h"
+#include "work.h"
+#include "store.h"
+#include "auth.h"
+#include "help.h"
+#include "work_challenge.h"
+
+#endif
