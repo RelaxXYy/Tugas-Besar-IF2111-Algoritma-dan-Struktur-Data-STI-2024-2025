16103d7ba105cdea7eaf1cab675493da108cf851
Gabriela Jennifer Sandy <167641624+GbrlJennie@users.noreply.github.com>
2024-11-25 07:47:59 +0700

    Update logout.c

--- logout.c
+++ logout.c
@@ -5,12 +5,7 @@
 
 void handle_logout(char *current_user) {
     printf(">> LOGOUT\n");
-    // Check if a user is logged in
-    if (current_user[0] == '\0') {
-        printf("Tidak ada pengguna yang sedang login. Silakan LOGIN terlebih dahulu.\n");
-        return;
-    }
-
+    
     // Inform the user about the logout
     printf("%s telah logout dari sistem PURRMART. Silakan REGISTER/LOGIN kembali untuk melanjutkan.\n", current_user);
 
