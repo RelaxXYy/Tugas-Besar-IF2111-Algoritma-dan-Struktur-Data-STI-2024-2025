1b4b84567048f48daf43440638abd99d4a278380
vincentiabelindaa <18223078@std.stei.itb.ac.id>
2024-12-17 20:43:32 +0700

    Update wishlist_swap.c

--- src/function/wishlist_swap.c
+++ src/function/wishlist_swap.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include "../include/all_library_headers.h"
 #include "../include/all_ADT_headers.h"
+
+// copystring ini keknya gaperlu ya??
 
 void copystring(char *dest, char *src) {
     int i = 0;
@@ -19,6 +20,11 @@ void wishlistswap(LinkedListChar *wishlist, int pos_1, int pos_2) {
         return;
     }
 
+    if (wishlist->First != NULL && wishlist->First->Next == NULL) {
+        printf("Hanya terdapat satu barang (%s) pada wishlist sehingga posisinya tidak dapat ditukar!\n", wishlist->First->Nama);
+        return;
+    }
+
     if (pos_1 == pos_2) {
         printf("Posisi yang dipilih sama! Tidak ada yang perlu ditukar\n");
         return;
@@ -32,17 +38,17 @@ void wishlistswap(LinkedListChar *wishlist, int pos_1, int pos_2) {
         P = P->Next;
         index++;
     }
-
+    
     if (P1 == NULL || P2 == NULL) {
         printf("Gagal menukar posisi! Salah satu atau kedua posisi tidak valid! \n");
         return;
     }
-
+    
     char temp[sizeof(P1->Nama)]; 
     copystring(temp, P1->Nama);
     copystring(P1->Nama, P2->Nama);
     copystring(P2->Nama, temp);
 
     printf("Berhasil menukar posisi %s dengan %s pada wishlist!\n", P1->Nama, P2->Nama);
-    printf("Urutan %s berubah menjadi %d, sebaliknya urutan %s menjadi %d\n", P1->Nama, pos2, P2->Nama, pos1);
+    printf("Urutan %s berubah menjadi %d, sebaliknya urutan %s menjadi %d\n", P1->Nama, pos_2, P2->Nama, pos_1);
 }
