52b4356d2699f67590593ea3ab5d5a83d9b0dc27
vincentiabelindaa <18223078@std.stei.itb.ac.id>
2024-12-17 20:14:55 +0700

    Update cart_add.c

--- src/function/cart_add.c
+++ src/function/cart_add.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 #include "../include/all_library_headers.h"
 #include "../include/all_ADT_headers.h"
 
-bool is_same_string(char *str1, char *str2) {
+int is_same_string(char *str1, char *str2) {
     int i = 0;
     while (str1[i] != '\0' && str2[i] != '\0') {
         if (str1[i] != str2[i]) {
-            return false;
+            return 0; 
         }
         i++;
     }
-    return (str1[i] == '\0' && str2[i] == '\0');
+    return (str1[i] == '\0' && str2[i] == '\0'); 
 }
 
-void cartadd(Map *keranjang, Store *toko, char *namabarang, int kuantitas) {
+void copy_string(char *dest, char *src) {
+    int i = 0;
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0'; 
+}
+
+int store_item_exists(Store *toko, char *namabarang) {
+    for (int i = 0; i < toko->Count; i++) {
+        if (is_same_string(toko->Items[i], namabarang)) {
+            return 1; 
+        }
+    }
+    return 0; 
+}
+
+void cart_add(Map *keranjang, Store *toko, char *namabarang, int kuantitas) {
     if (kuantitas <= 0) {
         printf("Kuantitas tidak valid!\n");
         return;
@@ -27,30 +45,24 @@ void cartadd(Map *keranjang, Store *toko, char *namabarang, int kuantitas) {
 
     int index = -1;
     for (int i = 0; i < keranjang->Count; i++) {
-        if (is_same_string(keranjang->Elements[i].Key, namabarang)) { 
+        if (is_same_string(keranjang->Elements[i].Key, namabarang)) {
             index = i;
             break;
         }
     }
 
     if (index == -1) { 
-        if (keranjang->Count < MaxEl) { 
-            int j = 0;
-            while (namabarang[j] != '\0') { 
-                keranjang->Elements[keranjang->Count].Key[j] = namabarang[j];
-                j++;
-            }
-            keranjang->Elements[keranjang->Count].Key[j] = '\0'; 
-
+        if (keranjang->Count < MaxEl) {
+            copy_string(keranjang->Elements[keranjang->Count].Key, namabarang);
             keranjang->Elements[keranjang->Count].Value = kuantitas;
             keranjang->Count++;
             printf("Berhasil menambahkan %d %s ke keranjang belanja!\n", kuantitas, namabarang);
         } else {
-            printf("Keranjang penuh :(\n");
+            printf("Keranjang penuh!\n");
         }
-    } else { 
+    } else { // barang sudah ada di keranjang, hanya kuantitas yang ditambah
         keranjang->Elements[index].Value += kuantitas;
         printf("Berhasil menambahkan %d %s ke keranjang belanja (UPDATE)!\n", kuantitas, namabarang);
     }
-    printf("Kembali ke menu utama\n");
+    printf("// Kembali ke menu utama\n");
 }
